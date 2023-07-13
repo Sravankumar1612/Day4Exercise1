d443c560d6f4401137f23b9362ebb414caa5f2ed
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-13 09:41:58 +0530

    intial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int preMarks;
+            Console.Write("Enter marks in Pre: ");
+            preMarks = int.Parse(Console.ReadLine());
+
+            if (preMarks > 60)
+            {
+                Console.Write("Enter marks in Final: ");
+                int finalMarks = int.Parse(Console.ReadLine());
+
+                if (finalMarks > 55)
+                {
+                    Console.WriteLine("Selected");
+                }
+                else
+                {
+                    Console.WriteLine("Fail in Final");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fail in Pre");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
